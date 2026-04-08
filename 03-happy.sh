2162c91176ccd81ba2f1f5540ea6f84be577a59b
YenDang03 <ydang@ggc.edu>
2026-04-08 14:37:19 -0400

    Force conflict from parallel

--- 03-happy.sh
+++ 03-happy.sh
@@ -6,7 +6,7 @@ read answer
 if [ "$answer" = "yes" ]; then
    echo "hmmmm gooood yess you are happy"
 else
-   echo "Don't forget to smile!"
+   echo "Don't forget to smile!!!"
 fi
 
 # here are the other string comparison operators
